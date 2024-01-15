3c7560d403640ee35ee209c838f81bec2747ec2b
ahmedabdeien <ahmedabdeien@gmail.com>
2024-01-15 14:01:19 +0200

    create pages  and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react"
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import SingIn from "./Components/Registry/SingIn/SingIn";
+import SingUp from './Components/Registry/SingUp/SingUp';
+import Profile from "./Components/Profile/Profile";
+import About from './Components/About/About';
 
 function App() {
-  let x = 10
-  console.log(x);
+  let x = 10 - 5 +40
+  console.log(x); 
   return (
-    <h1 className='bg-slate-400'>
-        ahmed abdeen
-      </h1>
+    <BrowserRouter>
+      <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/SingIn" element={<SingIn/>}/>
+         <Route path="/SingUp" element={<SingUp/>}/>
+         <Route path="/Profile" element={<Profile/>}/>
+         <Route path="/About" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
