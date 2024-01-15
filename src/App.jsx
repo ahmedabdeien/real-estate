0a936183871f364e27e80335e924e0efaed314a6
ahmedabdeien <ahmedabdeien@gmail.com>
2024-01-15 12:53:57 +0200

    install react js and tailwind css and create real estate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  let x = 10
+  console.log(x);
+  return (
+    <h1 className='bg-slate-400'>
+        ahmed abdeen
+      </h1>
+  )
+}
+
+export default App
