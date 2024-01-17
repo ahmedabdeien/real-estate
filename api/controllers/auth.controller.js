6540a32083659c596d2b003922f8dd025829e536
ahmedabdeien <ahmedabdeien@gmail.com>
2024-01-17 13:41:46 +0200

    add phone number

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,9 +2,9 @@ import { errorHandler } from "../Utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 export const signup = async (req ,res,next)=>{
-    const { username , email , password } = req.body;
+    const { username , email ,phoneNumber, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10)
-    const newUser = new User({username , email , password: hashedPassword});
+    const newUser = new User({username ,phoneNumber, email , password: hashedPassword});
     try{
         await newUser.save();
         res.status(201).json('use created successfully!');
