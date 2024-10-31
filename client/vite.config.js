8f9bd161115d733e6956e8ae0b2f6ec5ce43c938
Ahmed Abdeen <ahmedabdeien@gmail.com>
2024-10-31 16:21:43 +0300

    add pre 0.2

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://elsarh.netlify.app/',
         secure:false,
         changeOrigin: true,
       }
