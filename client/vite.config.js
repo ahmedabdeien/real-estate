4cec9163a61ba537965a22aa53b304e355ec89f5
Ahmed Abdeen <ahmedabdeien@gmail.com>
2025-05-24 18:43:52 +0300

    fix 2002

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,30 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-// دعم __dirname في ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://elsarh.netlify.app/', // أو أي API backend شغال
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: isProduction
+          ? 'https://elsarh.onrender.com' // 🔁 استخدم رابط Render هنا
+          : 'http://localhost:3000', // التطوير المحلي
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 })
