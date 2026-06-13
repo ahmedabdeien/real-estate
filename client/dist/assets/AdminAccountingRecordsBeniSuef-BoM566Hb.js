d7ba332973fa64e6ffe1383c8d895fc462b72fef
Ahmed Abdeen <ahmedabdeien@gmail.com>
2026-06-13 13:14:45 +0300

    build: update dist for Vercel deployment

---
+++ client/dist/assets/AdminAccountingRecordsBeniSuef-BoM566Hb.js
@@ -0,0 +1,1 @@
+import{j as r}from"./react-vendor-CQxFvlPW.js";import o from"./AdminAccountingRecords-67YbZs0P.js";import"./index-co9RG52S.js";import"./router-CjJ_MBYl.js";import"./framer-R0Lcz59e.js";import"./recharts-D0NnUiS9.js";import"./HelpCard-BZy14mcW.js";function s(){return r.jsx(o,{branch:"بني سويف",branchLabel:"فرع بني سويف"})}export{s as default};
